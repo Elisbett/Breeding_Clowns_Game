819dfbabb730b88e5adc256625b6313032571e7d
Daria Savtšenko <145145620+daria-sav@users.noreply.github.com>
2024-04-04 17:35:38 +0300

    FINAL

--- Bonuses.java
+++ Bonuses.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 public class Bonuses {
-    private int tearsForBonuses() {
+    public int tearsForBonuses(int level) {
         Random random = new Random();
         int tearsCost = (random.nextInt(5)) * level; //multiply by world lvl
         return tearsCost;
