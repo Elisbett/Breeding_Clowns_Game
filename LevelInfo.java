2516974a99bc7eade0fdc44099cfc2788f6fe687
Daria Savtšenko <145145620+daria-sav@users.noreply.github.com>
2024-04-04 17:39:12 +0300

    FINAL

--- LevelInfo.java
+++ LevelInfo.java
@@ -1,19 +1,22 @@
 import java.util.HashMap;
-
+// antud klass on koostatud, et säilitada infot iga klouni taseme kohta, et lihtsamalt seda kasutada
 public class LevelInfo {
     private String name;
     private int cost;
 
+    // konstruktor
     public LevelInfo(String name, int cost) {
         this.name = name;
         this.cost = cost;
     }
 
+    // getterid
     public String getName() {
         return name;
     }
 
     public int getCost() {
         return (int) Math.pow(cost, 3) + 5;
-    } //!! если будем менять формулу стоимости, то не забыть поменять sout в покупке первого
+    }
+    // Märkus iseendale, järgmiseks etapiks : !! если будем менять формулу стоимости, то не забыть поменять sout в покупке первого
 }
