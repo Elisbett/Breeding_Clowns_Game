c9708137c86251d07e539e2f2e2b81f3416b5d3c
Daria Savtšenko <145145620+daria-sav@users.noreply.github.com>
2024-04-04 11:07:49 +0300

    finish

--- WorldLevel.java
+++ WorldLevel.java
@@ -7,20 +7,29 @@ public class WorldLevel {
 
     private int level;
     private int minClownLevel;
-    private int maxClownLevel = minClownLevel + 5;
+    private int maxClownLevel;
     //boxes
-    private HashMap<Integer, Box> boxes = new HashMap<Integer, Box>();
+    private HashMap<Integer, BoxesClass> boxes = new HashMap<Integer, BoxesClass>();
+    private HashMap<Integer, ClownsClass> clownIndex;
     private Timer timer;
     //bonuses
-    private ArrayList<Bonuses>() bonuses = new ArrayList<Bonuses>();
+    private ArrayList<Bonuses> bonuses = new ArrayList<Bonuses>();
+    private int boxCounter = 0;
 
     public WorldLevel(int level, int minClownLevel) {
         this.level = level;
         this.minClownLevel = minClownLevel;
+        this.maxClownLevel = minClownLevel + 5;
+        this.clownIndex = new HashMap<>();
+        this.timer = new Timer();
     }
 
     public int getLevel() {
         return level;
+    }
+
+    public HashMap<Integer, BoxesClass> getBoxes() {
+        return boxes;
     }
 
     //boxes
@@ -34,33 +43,38 @@ public class WorldLevel {
             int generatedColwnsLevel = random.nextInt(2) + minClownLevel;
             return generatedColwnsLevel;
         }
-        if (maxOpenedClown == maxClownLevel ) {
+        if (maxOpenedClown == maxClownLevel) {
             Random random = new Random();
             int generatedColwnsLevel = random.nextInt(3) + minClownLevel;
             return generatedColwnsLevel;
-        }
+        } else { return 0;}
+
     }
-    
-    private void startBoxesGenerator() {
+
+    public HashMap<Integer, ClownsClass> getClownIndex() {
+        return clownIndex;
+    }
+
+    public void startBoxesGenerator() {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 generateBox();
             }
-        }, 0, 15 000 + (level - 1) * 5 000 ); //каждые 15 секунд + 5 секунд с повышением мира
+        }, 0, 15000 + (level - 1) * 5000 ); //каждые 15 секунд + 5 секунд с повышением мира
     } // надо в мейне вызвать этот метод для всех открытых миров
     // и при открытии новых запускать его для них
-    private void generateBox() {
+    public void generateBox() {
         if (boxes.size() < 6) {
-            Box box = new Box();
-            boxes.put(boxes.size(), box);
+            BoxesClass box = new BoxesClass();
+            boxes.put(boxCounter++, box);
             System.out.println("Kukkus uus karp!");
-            System.out.println("Praegu sul on " + boxes.size() + " karpi!");
+            System.out.println("Praegu sul on " + boxes.size() + " karp(i)!");
         }
     }
-    
-    //bonuses
-    private void bonusesGenerator() {
+
+    //bonuses + добавить, чтобы первый раз был не сразу при запуске
+    public void bonusesGenerator() {
         Random random = new Random();
         int time = random.nextInt(2);
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -68,7 +82,7 @@ public class WorldLevel {
             public void run() {
                 addBonusToList();
             }
-        } 0, 60 000 * 3 + time * 30 000;); // падает каждые 3-4 мин
+        }, 0, 60000 * 3 + time * 30000); // падает каждые 3-4 мин
     }
     private void addBonusToList() {
         if (bonuses.size() < 4) {
@@ -77,14 +91,15 @@ public class WorldLevel {
             System.out.println("Keegi valas popkorni maha( Tõsta!");
         }
     }
-    private void collectBonuses() {
+    public int collectBonuses() {
         int sum = 0;
         for (Bonuses element : bonuses) {
-            sum += element.tearsForBonuses();
+            sum += element.tearsForBonuses(level);
         }
         System.out.println("Sa korjasid " + bonuses.size() + " popkorni!");
         bonuses.clear();
         System.out.println("Popkorni maha tõstmisest sa teenisid " + sum + " pisaraid)");
+        return sum;
     }
     //добавить это в кошелек
 }
